7aa472972da6c2026e9dc4f2604e30f2fe176694
Satyam Warghat <warghatsatyam33@gmail.com>
2024-06-01 18:46:57 +0000

    Array

--- Javascript/array_learning.js
+++ Javascript/array_learning.js
@@ -4,17 +4,35 @@
 
 // using square brackets
 var arrayName = ['val1','val2','val3']
-console.log(arrayName,typeof arrayName);
+// console.log(arrayName,typeof arrayName);
 
 
 // using Array Objects
 var arrayName = new Array('val1','val2','val3','val4')
-console.log(arrayName);
+// console.log(arrayName);
 
 // Array can contains Heterogeneous element
-var arrayName= ["Val1",20,['s','a']]
-console.log(arrayName);
+var arrayName= ["Val1",20,['s','a'],undefined,null]
+// console.log(arrayName);
 
 // Accessing element in Array using indexing
-console.log(arrayName[0]);
-console.log(arrayName[5]);
+// console.log(arrayName[0]);
+// console.log(arrayName[5]);
+
+
+//console.log(arrayName.toString());
+
+
+// Iterating over arrays
+
+var arr = [10,20,30];
+
+// for (var i=0; i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+var items = ['apple','banana','orange']
+
+items.forEach(function(item){
+    console.log(item);
+})
