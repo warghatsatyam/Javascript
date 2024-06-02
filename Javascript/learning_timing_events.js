cbc0f1e8ab5e72898a35d3a3f26b35e1ca4fde63
Satyam Warghat <warghatsatyam33@gmail.com>
2024-06-02 18:27:44 +0000

    Timing Events

---
+++ Javascript/learning_timing_events.js
@@ -0,0 +1,27 @@
+// Timing events allow the execution of a piece of code at a specified time interval
+
+/*
+setTimeout()
+The setTimeout method is used to execute a certin piece of code after certain amount of time
+The setTimeout return a positive integer which represents the ID of the timer created
+
+setInterval()
+The method is used to execute a piece of code repeatedly with a fixed time interval between each call.
+
+
+clearTimeout()
+The clearTimeout() is used to cancel a timeout establised using setTimeout method.
+
+clearTimeout(timeoutId)
+timeoutId is the Id value returned by the setTimeout method
+
+clearInterval()
+
+The clearInterval() method is used to cancel the repeating timed action established using setInterval()
+method.
+
+clearInterval(intervalId)
+
+*/
+
+
