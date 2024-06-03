f9a4758ced298871d6861055060450aff17dc129
Satyam Warghat <warghatsatyam33@gmail.com>
2024-06-03 03:19:15 +0000

    Learning DOM

---
+++ Javascript/learning_dom.js
@@ -0,0 +1,9 @@
+var head1 = document.getElementById("one")
+head1.style.color="red";
+
+// We can use various api like
+// getElementById
+// getElementByClass
+// getElementByTagName
+// and many more and then change their property
+
