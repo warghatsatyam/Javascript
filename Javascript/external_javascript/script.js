9ff7660ddac13a181dbf179ae2b28710245beb2b
Satyam Warghat <warghatsatyam33@gmail.com>
2024-06-06 16:24:00 +0000

    External and Internal javascript

---
+++ Javascript/external_javascript/script.js
@@ -0,0 +1,14 @@
+
+
+console.log("Hello")
+
+function sayBye() {
+    alert("Bye")
+}
+
+function sayHello(){
+    alert("Hello")
+}
+
+var ele = document.getElementById("ele1")
+ele.addEventListener('click',sayHello());
